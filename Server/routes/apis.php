028c049aae5380d8c66b6a14a66e0428480214dc
joyagergess <gergessjoya@gmail.com>
2025-11-14 09:26:12 +0200

    Added the apis.php in routes

---
+++ Server/routes/apis.php
@@ -0,0 +1,23 @@
+<?php
+
+$apis = [
+    
+    '/users'           => ['controller' => 'UserController', 'method' => 'getUsers', 'type' => 'GET'],
+    '/user'            => ['controller' => 'UserController', 'method' => 'getUserByID', 'type' => 'GET'],
+    '/user/create'     => ['controller' => 'UserController', 'method' => 'createUser', 'type' => 'POST'],
+    '/user/update'     => ['controller' => 'UserController', 'method' => 'updateUser', 'type' => 'PUT'],
+    '/user/delete'     => ['controller' => 'UserController', 'method' => 'deleteUser', 'type' => 'DELETE'],
+
+ 
+    '/habits'          => ['controller' => 'HabitController', 'method' => 'getAll', 'type' => 'GET'],
+    '/habit'           => ['controller' => 'HabitController', 'method' => 'getOne', 'type' => 'GET'],
+    '/habit/create'    => ['controller' => 'HabitController', 'method' => 'create', 'type' => 'POST'],
+    '/habit/update'    => ['controller' => 'HabitController', 'method' => 'update', 'type' => 'PUT'],
+    '/habit/delete'    => ['controller' => 'HabitController', 'method' => 'delete', 'type' => 'DELETE'],
+
+    '/entries'         => ['controller' => 'EntriesController', 'method' => 'getAll', 'type' => 'GET'],
+    '/entry'           => ['controller' => 'EntriesController', 'method' => 'getOne', 'type' => 'GET'],
+    '/entry/create'    => ['controller' => 'EntriesController', 'method' => 'create', 'type' => 'POST'],
+    '/entry/update'    => ['controller' => 'EntriesController', 'method' => 'update', 'type' => 'PUT'],
+    '/entry/delete'    => ['controller' => 'EntriesController', 'method' => 'delete', 'type' => 'DELETE'],
+];
