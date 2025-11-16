80a13d603ad6b730302b85c0326baa3506dc2572
joyagergess <gergessjoya@gmail.com>
2025-11-16 20:33:24 +0200

    Created function to let the admin delete users

--- Frontend/Scripts/admin.js
+++ Frontend/Scripts/admin.js
@@ -12,14 +12,43 @@ document.addEventListener("DOMContentLoaded", async () => {
                 <td>${user.name}</td>
                 <td>${user.email}</td>
                 <td>
-                    <button class="editBtn">Edit</button>
-                    <button class="deleteBtn">Delete</button>
+                    <button class="deleteBtn" id=${user.id}>Delete</button>
                 </td>
             `;
             tbody.appendChild(row);
         });
+       handleDeleteButtons();
     } catch (error) {
         console.error("Error fetching users:", error);
     }
 });
+
+
+function handleDeleteButtons() {
+    document.querySelectorAll(".deleteBtn").forEach(btn => {
+        btn.addEventListener("click", async (e) => {
+        const id = e.target.id;
+        if (!confirm("Are you sure you want to delete this user?"))
+             return;
+
+        deleteUser(id);
+        });
+    });
+}
+
+async function deleteUser(id) {
+    try {
+        await axios.delete(`${base_url}/user/delete`, {
+            data: { id: id }  
+        });
+
+        alert("User deleted");
+        location.reload();
+
+    } catch (error) {
+        console.error(error);
+        alert("Failed to delete user.");
+    }
+}
+
 
