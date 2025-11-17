6f40af59be836570a2b1ca9375fc91cb3c304079
joyagergess <gergessjoya@gmail.com>
2025-11-17 10:33:56 +0200

    Fetched habits data and fixed delete button

--- Frontend/Scripts/admin.js
+++ Frontend/Scripts/admin.js
@@ -14,20 +14,20 @@ document.addEventListener("DOMContentLoaded", async () => {
                 <td>${user.email}</td>
                 <td>${user.created_at}</td>
                 <td>
-                    <button class="deleteBtn" id=${user.id}>Delete</button>
+                    <button class="deleteUserBtn" id=${user.id}>Delete</button>
                 </td>
             `;
             tbody.appendChild(row);
         });
-       handleDeleteButtons();
+       handleDeleteButtonUser();
     } catch (error) {
         console.error("Error fetching users:", error);
     }
 });
 
 
-function handleDeleteButtons() {
-    document.querySelectorAll(".deleteBtn").forEach(btn => {
+function handleDeleteButtonUser() {
+    document.querySelectorAll(".deleteUserBtn").forEach(btn => {
         btn.addEventListener("click", async (e) => {
         const id = e.target.id;
         if (!confirm("Are you sure you want to delete this user?"))
@@ -41,7 +41,7 @@ function handleDeleteButtons() {
 async function deleteUser(id) {
     try {
         await axios.delete(`${base_url}/user/delete`, {
-            data: { id: id }  
+            data: { id }    
         });
 
         alert("User deleted");
@@ -53,4 +53,62 @@ async function deleteUser(id) {
     }
 }
 
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const response = await axios.get(`${base_url}/habits`);
+        const habits = response.data.data;
+
+        const tbody = document.querySelector("#habitsTable tbody");
+        tbody.innerHTML = ""; 
+
+        habits.forEach(habit => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${habit.id}</td>
+                <td>${habit.user_id}</td>
+                <td>${habit.name}</td>
+                <td>${habit.category}</td>
+                <td>${habit.target}</td>
+                <td>${habit.status}</td>
+                <td>${habit.created_at}</td>
+                <td>
+                    <button class="deleteHabitBtn" id=${habit.id}>Delete</button>
+                </td>
+            `;
+            tbody.appendChild(row);
+        });
+       handleDeleteButtonHabit();
+    } catch (error) {
+        console.error("Error fetching habits:", error);
+    }
+});
+
+
+function handleDeleteButtonHabit() {
+    document.querySelectorAll(".deleteHabitBtn").forEach(btn => {
+        btn.addEventListener("click", async (e) => {
+        const id = e.target.id;
+        if (!confirm("Are you sure you want to delete this habit?"))
+             return;
+
+        deleteHabit(id);
+        });
+    });
+}
+
+async function deleteHabit(id) {
+    try {
+     await axios.delete(`${base_url}/habit/delete?id=${id}`);
+   
+
+        alert("habit deleted");
+        location.reload();
+
+    } catch (error) {
+        console.error(error);
+        alert("Failed to delete habit.");
+    }
+}
 
